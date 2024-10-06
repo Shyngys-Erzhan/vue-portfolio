f53c87fdb137c7758fd4ce0083faa7c69fcfad93
Shyngys-Erzhan <146824281+Shyngys-Erzhan@users.noreply.github.com>
2024-10-06 05:18:38 +0500

    FEAT настройка сайта, функционал с смены тем

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ProfileSection from "@/pages/Home/ProfileSection.vue";
+</script>
+
+<template>
+  <div>
+    <ProfileSection />
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the home page of the portfolio.</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #007bff;
+}
+</style>
---
+++ src/pages/Home/ProfileSection.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+<template>
+  <div class="content">
+    <img src="@/assets/images/profile.jpg" alt="Profile image" />
+    <div class="divider"></div>
+
+    <div class="description">
+      <h1>Shyngys Erzhan</h1>
+      <p>afsnkjasnd</p>
+      <button>mfdskl</button>
+    </div>
+  </div>
+</template>
+<style scoped>
+.content {
+  width: 1440px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 50px auto;
+}
+img {
+  width: 350px;
+  height: 350px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin: 0 auto;
+  margin: 0;
+}
+.divider {
+  height: 350px;
+  width: 2px;
+  background-color: red;
+}
+.description {
+  width: 350px;
+}
+</style>
